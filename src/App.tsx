495f7a34b089919886e4dac71797550a10bc9249
Parveen Kaur <parveenkaur1045@gmail.com>
2024-08-20 20:02:06 -0600

    added useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 
-
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  const [enteredName ,setEnteredName] =useState("");
+  //useState read the piece of code and relaod the page 
   
 
   return (
     <div>
-      <h1>Hello ComMIT</h1>
+      <h1>Hello {enteredName}</h1>
+      <input onChange = {(e) => {
+        setEnteredName(e.target.value)}}type="text" placeholder='name'></input>
+
+
+      {/* <button onClick={() =>{
+        setEnteredName ("kaur");
+      }}>Click here</button> */}
     </div>
     
   );
