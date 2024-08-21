3e534ba1d88a8d066ff2ba7d1e1a7367288049b8
Parveen Kaur <parveenkaur1045@gmail.com>
2024-08-20 21:01:06 -0600

    add registration form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,46 @@
 import { useState } from 'react';
 import './App.css'
 
-function App() {
 
-  const [enteredName ,setEnteredName] =useState("");
+
+function App() {
+  const [firstName, setFirstName] =  useState("");
+  const [lastName, setLastName] = useState("");
+  const [age ,setAge] = useState(Number);
+
+
+  //const [enteredName ,setEnteredName] =useState("");
   //useState read the piece of code and relaod the page 
+ // const [firstName ] =useState("");
   
 
   return (
     <div>
-      <h1>Hello {enteredName}</h1>
+      <h1>Registration Form</h1>
+      <label>First Name</label> <br></br>
+      <input onChange = {(e) =>{
+        setFirstName(e.target.value)}
+      }
+        type="text" placeholder='firstName' ></input><br></br>
+
+      <label>Last Name</label><br></br>
+      <input onChange={(e) => {
+        setLastName(e.target.value)
+      }} type ="text" placeholder='lastName' ></input><br></br>
+
+      <label>Age</label><br></br>
       <input onChange = {(e) => {
-        setEnteredName(e.target.value)}}type="text" placeholder='name'></input>
+        setAge(parseInt(e.target.value))}}
+        type="number" placeholder ="age"></input>
 
+      <p>"Hello {firstName} {lastName}! Are you Sure you are {age} years Old"</p>
 
-      {/* <button onClick={() =>{
-        setEnteredName ("kaur");
-      }}>Click here</button> */}
+      
     </div>
+
+  
     
   );
-}
+};
 
-export default App
+export default App;
